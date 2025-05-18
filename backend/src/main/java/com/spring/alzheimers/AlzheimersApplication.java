9bb064082f693d36ebff8aafabf8e172a777e992
MichaelAlex03 <michaelalex03@outlook.com>
2025-05-18 15:34:58 -0500

    setting up spring

---
+++ backend/src/main/java/com/spring/alzheimers/AlzheimersApplication.java
@@ -0,0 +1,21 @@
+package com.spring.alzheimers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class AlzheimersApplication {
+
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		
+		SpringApplication.run(AlzheimersApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spring/alzheimers/AlzheimersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.alzheimers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlzheimersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

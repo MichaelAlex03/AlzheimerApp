c0908aced56de082ce0a18f2937d56e4fb397482
MichaelAlex03 <michaelalex03@outlook.com>
2025-05-18 00:07:52 -0500

    layout update

--- frontend/app/(auth)/(help)/_layout.tsx
+++ frontend/app/(auth)/(help)/_layout.tsx
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
-const _layout = () => {
+const SignUpHelpLayout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Stack>
+      <Stack.Screen name='SignUpHelp' options={{ headerShown: false }} />
+    </Stack>
   )
 }
 
-export default _layout
+export default SignUpHelpLayout
 
 const styles = StyleSheet.create({})
